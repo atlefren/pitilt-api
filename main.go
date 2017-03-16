f42956d0e822ae1168e413bcfbc7587f6649c4f9
Atle Frenvik Sveen <atle@frenviksveen.net>
2017-03-16 21:18:39 +0100

    gofmt

--- main.go
+++ main.go
@@ -1,171 +1,168 @@
 package main
 
 import (
-    "github.com/gorilla/mux"
-    _ "github.com/lib/pq"
-    "github.com/jmoiron/sqlx"
-    "time"
-    "log"
-    "encoding/json"
-    "net/http"
-    "errors"
-    "database/sql"
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"time"
 )
 
 type Env struct {
-    db *sqlx.DB
+	db *sqlx.DB
 }
 
 func readMeasurements(user string, color string, db *sqlx.DB) ([]Measurement, error) {
-    measurements := []Measurement{}
-    err := db.Select(&measurements, "SELECT color, ts, gravity, temperature FROM measurement WHERE color = $1 AND login = $2  ORDER BY ts", color, user)
-    return measurements, err
+	measurements := []Measurement{}
+	err := db.Select(&measurements, "SELECT color, ts, gravity, temperature FROM measurement WHERE color = $1 AND login = $2  ORDER BY ts", color, user)
+	return measurements, err
 }
 
 func readLastMeasurement(user string, color string, db *sqlx.DB) (Measurement, error) {
-    measurement := Measurement{}
-    err := db.Get(&measurement, "SELECT color, ts, gravity, temperature FROM measurement WHERE color = $1 AND login = $2 ORDER BY ts DESC LIMIT 1", color, user)
-    return measurement, err
+	measurement := Measurement{}
+	err := db.Get(&measurement, "SELECT color, ts, gravity, temperature FROM measurement WHERE color = $1 AND login = $2 ORDER BY ts DESC LIMIT 1", color, user)
+	return measurement, err
 }
 
 func saveMeasurements(measurements []Measurement, user string, db *sqlx.DB) error {
-    tx := db.MustBegin()
-    for _, measurement := range measurements {
-        measurement.User = user
-        tx.NamedExec("INSERT INTO measurement (color, gravity, temperature, ts, login) VALUES (:color, :gravity, :temperature, :ts, :login)", &measurement)
-    }
-    tx.Commit()
-    return nil
+	tx := db.MustBegin()
+	for _, measurement := range measurements {
+		measurement.User = user
+		tx.NamedExec("INSERT INTO measurement (color, gravity, temperature, ts, login) VALUES (:color, :gravity, :temperature, :ts, :login)", &measurement)
+	}
+	tx.Commit()
+	return nil
 }
-
 
 func parseMeasurements(r *http.Request) []Measurement {
-    decoder := json.NewDecoder(r.Body)
-    var measurements []Measurement
-    err := decoder.Decode(&measurements)
-    if err != nil {
-        panic(err)
-    }
-    defer r.Body.Close()
-    return  measurements
+	decoder := json.NewDecoder(r.Body)
+	var measurements []Measurement
+	err := decoder.Decode(&measurements)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	return measurements
 }
 
-func getUser(r *http.Request, db *sqlx.DB) (string, error){
-    key := r.Header.Get("X-PYTILT-KEY")
+func getUser(r *http.Request, db *sqlx.DB) (string, error) {
+	key := r.Header.Get("X-PYTILT-KEY")
 
-    var id string
-    err := db.Get(&id, "SELECT id FROM login WHERE key = $1", key)
+	var id string
+	err := db.Get(&id, "SELECT id FROM login WHERE key = $1", key)
 
-    if (err == sql.ErrNoRows) {
-        return "", errors.New("unknown key")
-    }
-    return id, err
+	if err == sql.ErrNoRows {
+		return "", errors.New("unknown key")
+	}
+	return id, err
 }
 
 func (env *Env) addData(w http.ResponseWriter, r *http.Request) {
 
-    user, err := getUser(r, env.db)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusForbidden)
-        return
-    }
+	user, err := getUser(r, env.db)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
 
-    measurements := parseMeasurements(r)
+	measurements := parseMeasurements(r)
 
-    err = saveMeasurements(measurements, user, env.db)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	err = saveMeasurements(measurements, user, env.db)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    //w.WriteHeader(http.StatusCreated)
-    w.WriteHeader(http.StatusOK)
+	//w.WriteHeader(http.StatusCreated)
+	w.WriteHeader(http.StatusOK)
 }
 
 func (env *Env) getAllData(w http.ResponseWriter, r *http.Request) {
 
-    //todo: the user should come from an OAuth token, not the key
-    user, err := getUser(r, env.db)
+	//todo: the user should come from an OAuth token, not the key
+	user, err := getUser(r, env.db)
 
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusForbidden)
-        return
-    }
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
 
-    vars := mux.Vars(r)
-    color := vars["color"]
+	vars := mux.Vars(r)
+	color := vars["color"]
 
-    measurements, err := readMeasurements(user, color, env.db)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	measurements, err := readMeasurements(user, color, env.db)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    jsonData, err := json.Marshal(measurements)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(jsonData)
+	jsonData, err := json.Marshal(measurements)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
 
 func (env *Env) getLatestData(w http.ResponseWriter, r *http.Request) {
 
-    //todo: the user should come from an OAuth token, not the key
-    user, err := getUser(r, env.db)
+	//todo: the user should come from an OAuth token, not the key
+	user, err := getUser(r, env.db)
 
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusForbidden)
-        return
-    }
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusForbidden)
+		return
+	}
 
-    vars := mux.Vars(r)
-    color := vars["color"]
+	vars := mux.Vars(r)
+	color := vars["color"]
 
-    measurement, err := readLastMeasurement(user, color, env.db)
-    if err != nil {
-        if (err == sql.ErrNoRows) {
-            http.Error(w, "Color not found", http.StatusNotFound)
-        } else {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-        return
-    }
+	measurement, err := readLastMeasurement(user, color, env.db)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.Error(w, "Color not found", http.StatusNotFound)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
 
-    jsonData, err := json.Marshal(measurement)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(jsonData)
+	jsonData, err := json.Marshal(measurement)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
 
 func main() {
-    var err error
-    db, err := sqlx.Open("postgres", "postgres://dvh2user:pass@localhost:15432/dvh2")
-    if err != nil {
-        log.Fatalln(err)
-    }
-    env := &Env{db: db}
+	var err error
+	db, err := sqlx.Open("postgres", "postgres://dvh2user:pass@localhost:15432/dvh2")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	env := &Env{db: db}
 
+	//TODO Add CORS support
+	r := mux.NewRouter()
 
-    //TODO Add CORS support
-    r := mux.NewRouter()
+	//todo: add route that lets a new oauth token generate a key
+	r.HandleFunc("/", http.HandlerFunc(env.addData)).Methods("POST")
+	r.HandleFunc("/data/all/{color}", http.HandlerFunc(env.getAllData)).Methods("GET")
+	r.HandleFunc("/data/latest/{color}", http.HandlerFunc(env.getLatestData)).Methods("GET")
 
-    //todo: add route that lets a new oauth token generate a key
-    r.HandleFunc("/", http.HandlerFunc(env.addData)).Methods("POST")
-    r.HandleFunc("/data/all/{color}", http.HandlerFunc(env.getAllData)).Methods("GET")
-    r.HandleFunc("/data/latest/{color}", http.HandlerFunc(env.getLatestData)).Methods("GET")
-
-
-    s := &http.Server{
-        Addr:           ":8080",
-        Handler:        r,
-        ReadTimeout:    10 * time.Second,
-        WriteTimeout:   10 * time.Second,
-        MaxHeaderBytes: 1 << 20,
-    }
-    log.Fatal(s.ListenAndServe())
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
